c09b3c4c441778be5b9e224c3f3ff6bce5cd7e50
ArmenG <armeng2007@gmail.com>
2022-05-07 15:02:38 -0800

    Made functional likes for posts and comments

--- pysta/post/urls.py
+++ pysta/post/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name="home"),
-    path('p/<pk>', views.post_detail_view, name="post-detail")
+    path('p/<id>', views.post_detail_view, name="post-detail"),
+    path('<id>/like/', views.like, name="post-like"),
+    path('p/<id>/like/', views.like_detail, name="post-like-detail"),
+    path('p/<id>/<comment_id>/like', views.comment_like, name="comment-like")
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pysta/post/views.py
+++ pysta/post/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import post, comment
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url="/admin")
 def home(request):
     for i in post.objects.all():
         comments = i.comment_set.all()
@@ -12,11 +15,53 @@ def home(request):
 
     return render(request, 'post/home.html', context)
 
-def post_detail_view(request, pk):
-    post_x = post.objects.all().filter(id=pk)[0]
+def post_detail_view(request, id):
+    post_x = post.objects.all().filter(id=id)[0]
     comments = post_x.comment_set.all()
     context = {
         'post':post_x,
         'comments':comments
     }
     return render(request, 'post/post_detail.html', context)
+
+def like(request, id):
+    post_x = post.objects.all().filter(id=id)[0]
+    already_liked = False 
+    for i in post_x.user_liked.all():
+        if i == request.user:
+            already_liked = True 
+    
+    if already_liked == False:
+        post_x.user_liked.add(request.user)
+    else:
+        post_x.user_liked.remove(request.user)
+
+    return redirect('home')
+
+def like_detail(request, id):
+    post_x = post.objects.all().filter(id=id)[0]
+    already_liked = False 
+    for i in post_x.user_liked.all():
+        if i == request.user:
+            already_liked = True 
+    
+    if already_liked == False:
+        post_x.user_liked.add(request.user)
+    else:
+        post_x.user_liked.remove(request.user)
+
+    return redirect('post-detail', id)
+    
+def comment_like(request, id, comment_id):
+    comment_x = comment.objects.all().filter(id=comment_id)[0]
+    already_liked = False 
+    for i in comment_x.user_liked.all():
+        if i == request.user:
+            already_liked = True 
+    
+    if already_liked == False:
+        comment_x.user_liked.add(request.user)
+    else:
+        comment_x.user_liked.remove(request.user)
+
+    return redirect('post-detail', id) 
