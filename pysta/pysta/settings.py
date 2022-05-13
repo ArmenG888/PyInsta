b5d5ddf3039d55f59d92b26f5e61f7b0a4d498cf
ArmenG <52843550+ArmenG888@users.noreply.github.com>
2022-05-13 09:58:02 -0700

    Update settings.py

--- pysta/pysta/settings.py
+++ pysta/pysta/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3n_b(s2$7dm9uke8f5-uo$ylz^so#9$82845935(#f3elp2o*t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
